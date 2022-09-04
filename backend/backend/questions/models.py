b0d5fabbb4af38d98c6fe82efb5f7c1afdab0a8c
mac <macbook@mohamed-2.local>
2022-09-04 21:38:31 +0100

    set up the basic models

---
+++ backend/backend/questions/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from themes.models import Theme
+# Create your models here.
+class Question(models.Model):
+    choices = (
+        ('easy','easy'),
+        ('medium','medium'),
+        ('hard','hard')
+    )
+    theme = models.ForeignKey(Theme,on_delete=models.CASCADE)
+    text = models.CharField(max_length=100)
+    right_answer = models.OneToOneField('Answer',on_delete=models.CASCADE)
+    wrong_answer = models.OneToOneField('Answer',related_name='default',on_delete=models.CASCADE)
+    difficulty = models.CharField(max_length =10,choices=choices , default='easy')
+    def __str__(self):
+        return self.text
+class Answer(models.Model):
+    text = models.CharField(max_length=100)
+    def __str__(self):
+        return self.text
